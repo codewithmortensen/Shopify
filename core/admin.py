45a7cc06332f828025de3aa4c8fb0e8b856ddf10
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-10-31 14:46:50 -0400

    Create the User Admin Panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    super_fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ('first_name', 'last_name', 'email', "username", "password1", "password2"),
+            },
+        ),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        elif request.user.is_superuser:
+            return self.super_fieldsets
+        elif request.user.is_staff:
+            return self.fieldsets
+
+        return super().get_fieldsets(request, obj)
