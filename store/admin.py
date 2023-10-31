0ea9db85ed38c30372f7e71d225aa2c22669a2ce
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-10-31 14:41:51 -0400

    Create the Customer Admin Panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,39 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.urls import reverse
+from django.db.models.aggregates import Count
+from django.utils.http import urlencode
+from django.utils.html import format_html
+from . import models
 
-# Register your models here.
+
+class AddressInline(admin.TabularInline):
+    model = models.Address
+    extra = 0
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    list_display = [
+        'customer_id', 'first_name', 'last_name',
+        'email', 'membership', 'order_count'
+    ]
+    list_filter = ['membership']
+    list_editable = ['membership']
+    inlines = [AddressInline]
+    list_per_page = 10
+
+    @admin.display(ordering='order_count')
+    def order_count(self, customer: models.Customer):
+        url = reverse('admin:store_order_changelist') + '?' + urlencode({
+            'customer_id': str(customer.id)
+        })
+        return format_html('<a href={}>{}</a>', url, customer.order_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            order_count=Count('orders')
+        )
