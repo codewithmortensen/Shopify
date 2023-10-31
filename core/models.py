30d5781d19a15d38335e748e4e320c430514746b
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-10-31 13:57:56 -0400

    Extend Django default user Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        max_length=255,
+        validators=[MinLengthValidator(3)]
+    )
+    last_name = models.CharField(
+        max_length=255,
+        validators=[MinLengthValidator(3)]
+    )
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
