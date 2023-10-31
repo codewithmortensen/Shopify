08bd6d2bafb6d1204d96c78a973179957d42df93
Mortensen Ulysse <codewithmortensen@gmail.com>
2023-10-31 14:13:29 -0400

    Create the Customer Model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from Shopify.settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class Customer(models.Model):
+    customer = models.OneToOneField(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    membership_gold = 'G'
+    membership_silver = 'S'
+    membership_bronze = 'B'
+
+    membership_status = [
+        (membership_bronze, 'Bronze'),
+        (membership_silver, 'Silver'),
+        (membership_gold, 'Gold')
+    ]
+
+    membership = models.CharField(
+        max_length=1, choices=membership_status, default=membership_bronze)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['customer__first_name', 'customer__last_name']
